f1f023a300f2a4808fa76a95bc53bee33d8faa29
man2015yes@163.com <man18629079232>
2018-02-02 18:27:42 +0800

    完善项目创建

---
+++ src/main/java/com/example/demo/controls/loginControl.java
@@ -0,0 +1,10 @@
+package com.example.demo.controls;
+
+import com.example.demo.service.impl.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class loginControl {
+
+}
