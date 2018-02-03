c2484c8f951d9819a00a3590a9e497447d947f93
man2015yes@163.com <man18629079232>
2018-02-04 01:31:45 +0800

    完善项目创建

--- src/main/java/com/example/demo/Uitl/Common.java
+++ src/main/java/com/example/demo/Uitl/Common.java
@@ -2,6 +2,10 @@ package com.example.demo.Uitl;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Common {
     /***
@@ -14,5 +18,45 @@ public class Common {
             return null;
         }
         return request.getSession();
+    }
+
+    /***
+     * 获取当前时间
+     * @return
+     */
+    public static String getDate(){
+        Date date = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return simpleDateFormat.format(date);
+    }
+
+    /***
+     * 返回数据存入时间格式时分秒
+     * @return
+     * @throws ParseException
+     */
+    public static Time getSqlTime() throws ParseException {
+        String s = getDate();
+        SimpleDateFormat sp = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        Date date=sp.parse(s);
+        return new java.sql.Time(date.getTime());
+    }
+
+    /***
+     * 返回数据库存入时间，年月日
+     * @return
+     * @throws ParseException
+     */
+    public static java.sql.Date getSqlDate() throws ParseException {
+        String s = getDate();
+        SimpleDateFormat sp = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        Date date=sp.parse(s);
+        return new java.sql.Date(date.getTime());
+    }
+
+    public static Time getCheckTime(String time) throws ParseException {
+        SimpleDateFormat sp = new SimpleDateFormat("HH:mm:ss");
+        Date date = sp.parse(time);
+        return new Time(date.getTime());
     }
 }
---
+++ src/main/java/com/example/demo/controls/IndexControl.java
@@ -0,0 +1,13 @@
+package com.example.demo.controls;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class IndexControl {
+
+    public String UserSign(Integer id){
+        return null;
+    }
+}
